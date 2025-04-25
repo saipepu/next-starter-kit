9e650c1403c61c65735d7da29e15eeb9255946b4
saipepu <saipepu.mdy257@gmail.com>
2025-04-25 12:07:36 +0700

    update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/providers/theme-provider";
+import { SessionProvider } from "next-auth/react";
+import Providers from "@/providers";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,14 +29,10 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased w-full h-full`}
-      >
-        <ThemeProvider
-          attribute="class"
-          enableSystem
-          disableTransitionOnChange
         >
-          {children}
-        </ThemeProvider>
+          <Providers>
+            {children}
+          </Providers>
       </body>
     </html>
   );
