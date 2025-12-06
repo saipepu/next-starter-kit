78d100961e9d05bdf1b29d1a62b7d629a43f1cf2
saipepu <saipepu.mdy257@gmail.com>
2025-12-06 15:45:35 +0700

    feat: integrate user session handling in DocsIntro component

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,20 +1,22 @@
-"use client";
-import { Button } from '@/components/ui/button'
-import { signOut } from 'next-auth/react'
-import React from 'react'
+"use client"
+import { Button } from "@/components/ui/button"
+import { signOut, useSession } from "next-auth/react"
+import React from "react"
 
 const DocsIntro = () => {
+  const { data: session } = useSession()
+  console.log("DocsIntro session", session)
+  console.log(session?.user?.accessToken)
+  console.log(session?.user?.refreshToken)
+
   return (
-    <div className='w-full min-h-full'>
-      <h1 className='text-4xl font-bold mt-5'>Introduction</h1>
-      <p className='text-gray-500 mt-2'>
-        This is the introduction to the documentation. Here you can find information about the project, its features, and how to get started.
+    <div className="w-full min-h-full">
+      <h1 className="text-4xl font-bold mt-5">Introduction</h1>
+      <p className="text-gray-500 mt-2">
+        This is the introduction to the documentation. Here you can find information about
+        the project, its features, and how to get started.
       </p>
-      <Button
-        variant='default'
-        className='mt-4'
-        onClick={() => signOut()}
-      >
+      <Button variant="default" className="mt-4" onClick={() => signOut()}>
         Sign Out
       </Button>
     </div>
