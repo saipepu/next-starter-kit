8c75f6068c88ddb7c292a4be3a9318fe97572192
saipepu <saipepu.mdy257@gmail.com>
2025-12-06 15:45:46 +0700

    feat: add AuthModel with sign-in and token renewal types

---
+++ domain/models/AuthModel.ts
@@ -0,0 +1,35 @@
+export enum AuthMethods {
+  Google = "google",
+  Credentials = "credentials",
+}
+
+export type SignInRequestDto = {
+  method: AuthMethods.Google;
+  providerToken: string
+} | {
+  method: AuthMethods.Credentials;
+  email: string;
+  password: string;
+}
+
+export type SignInResponseDto = {
+  success: boolean;
+  message: {
+    access_token: string;
+    refresh_token: string;
+    access_token_expires_in: number;
+    otp_expiry?: number;
+    email?: string;
+    is_verified?: boolean;
+    is_first_time_user?: boolean;
+    otp?: string;
+  };
+}
+
+export type RenewAccessTokenRequest = {
+  refresh_token: string;
+}
+
+export type RenewAccessTokenResponse = {
+  success: boolean;
+}
